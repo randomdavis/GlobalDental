c497f589921b8f37d3f59271f49ba7eb1ebada2c
Random Davis <random.davis@gmail.com>
2016-10-23 16:40:27 -0700

    Adds Surface Selection dialog

---
+++ GlobalDental/GlobalDentalUI/SurfaceSelection.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace GlobalDentalUI
+{
+    partial class SurfaceSelection
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SurfaceSelection));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.OKButton = new System.Windows.Forms.Button();
+            this.MesialCheckBox = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.checkBox4, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.checkBox3, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.checkBox2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.checkBox1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.OKButton, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.MesialCheckBox, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(172, 149);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // OKButton
+            // 
+            this.OKButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.OKButton.Location = new System.Drawing.Point(89, 101);
+            this.OKButton.Name = "OKButton";
+            this.OKButton.Size = new System.Drawing.Size(80, 45);
+            this.OKButton.TabIndex = 0;
+            this.OKButton.Text = "Select";
+            this.OKButton.UseVisualStyleBackColor = true;
+            // 
+            // MesialCheckBox
+            // 
+            this.MesialCheckBox.AutoSize = true;
+            this.MesialCheckBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MesialCheckBox.Location = new System.Drawing.Point(0, 0);
+            this.MesialCheckBox.Margin = new System.Windows.Forms.Padding(0);
+            this.MesialCheckBox.Name = "MesialCheckBox";
+            this.MesialCheckBox.Size = new System.Drawing.Size(86, 49);
+            this.MesialCheckBox.TabIndex = 1;
+            this.MesialCheckBox.Text = "Mesial";
+            this.MesialCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.checkBox1.Location = new System.Drawing.Point(86, 0);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(86, 49);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Lingual";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.checkBox2.Location = new System.Drawing.Point(0, 49);
+            this.checkBox2.Margin = new System.Windows.Forms.Padding(0);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(86, 49);
+            this.checkBox2.TabIndex = 3;
+            this.checkBox2.Text = "Occlusal";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.checkBox3.Location = new System.Drawing.Point(86, 49);
+            this.checkBox3.Margin = new System.Windows.Forms.Padding(0);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(86, 49);
+            this.checkBox3.TabIndex = 4;
+            this.checkBox3.Text = "Buccal";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.checkBox4.Location = new System.Drawing.Point(0, 98);
+            this.checkBox4.Margin = new System.Windows.Forms.Padding(0);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(86, 51);
+            this.checkBox4.TabIndex = 5;
+            this.checkBox4.Text = "Distal";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // SurfaceSelection
+            // 
+            this.AcceptButton = this.OKButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(172, 149);
+            this.ControlBox = false;
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "SurfaceSelection";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Surfaces";
+            this.TopMost = true;
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button OKButton;
+        private System.Windows.Forms.CheckBox MesialCheckBox;
+    }
+}
